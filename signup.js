fd68fd14bc91f62ed542c247cd6e10e0bbb5a30b
Yan Feng Lin <ylin013@citymail.cuny.edu>
2019-10-23 01:01:12 -0400

    update signup.js

--- signup.js
+++ signup.js
@@ -2,22 +2,6 @@ import React, { Component } from 'react';
 import './signup.css';
 
 class signup extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            firstName: null,
-            lastName: null,
-            email: null,
-            password: null,
-            formErrors: {
-                firstName: "",
-                lastName: "",
-                email: "",
-                password: "",
-            }
-        };
-    }
     render() {
         return (
             <div className="wrapper">
@@ -27,23 +11,23 @@ class signup extends Component {
                     <form onSubmit={this.handleSubmit} noValidate>
                         <div className="firstName">
                             <label htmlFor="firstName">First Name:</label>
-                            <input type="text" className="" placeholder="Enter first name"
-                                type="text" name="firstName" formNoValidate onChange={this.handleChange} />
+                            <input type="text" name="firstName" placeholder="Enter first name"
+                                formNoValidate onChange={this.handleChange} />
                         </div>
                         <div className="lastName">
                             <label htmlFor="lastName">Last Name:</label>
-                            <input type="text" className="" placeholder="Enter last name"
-                                type="text" name="lastName" formNoValidate onChange={this.handleChange} />
+                            <input type="text" name="lastName" placeholder="Enter last name"
+                                formNoValidate onChange={this.handleChange} />
                         </div>
                         <div className="email">
                             <label htmlFor="email">Email:</label>
-                            <input type="text" className="" placeholder="Enter email"
-                                type="text" name="email" formNoValidate onChange={this.handleChange} />
+                            <input type="text" name="email" placeholder="Enter email"
+                                formNoValidate onChange={this.handleChange} />
                         </div>
                         <div className="password">
-                            <label htmlFor="email">Password:</label>
-                            <input type="text" className="" placeholder="Enter password"
-                                type="text" name="email" formNoValidate onChange={this.handleChange} />
+                            <label htmlFor="password">Password:</label>
+                            <input type="password" name="password" placeholder="Enter password"
+                                formNoValidate onChange={this.handleChange} />
                         </div>
                         <div className="createAccount">
                             <button type="submit">Create Account</button>
