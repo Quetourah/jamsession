ffe4969875c34cc9dee0b3eb80a9fda062d7bab0
Excel Espina <espinaexcel@gmail.com>
2019-10-27 13:44:01 -0400

    refactored the main screen

--- app/src/App.js
+++ app/src/App.js
@@ -7,12 +7,13 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      data: ''
+      data: '',
+      connected: false
     };
   }
 
   sendToServer = (cmd) => {
-    const response = axios.post(
+    axios.post(
       'http://localhost:5000/hello',
       { data: cmd },
       { headers: { 'ContentType': 'application/json' } },
@@ -22,34 +23,71 @@ class App extends Component {
       console.log(err)
     })
   }
+  renderSwitch() {
+    if (this.state.connected) {
+      return {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "50vw"
+      }}
+    else {
+      return {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh"
+      }}
+  }
 
-  render() {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100vh"
-        }}
-      >
+  renderLandingMessage() {
+    if (!this.state.connected) {
+      return (
+        <div style={{
+                alignContent: "center",
+                marginLeft: "auto",
+                marginRight: "auto"
+            }}>
+          <h2>Welcome to Jam Session (Python)</h2><br/>
+            <button style={{textAlign: "center"}} onClick={()=>{this.setState({connected: true})}}>Connect</button>
+          <p>Click the Connect button to launch a session.</p>
+        </div>
+      )
+    }
+  }
+
+  renderMainScreen() {
+    if (this.state.connected) {
+      return (
         <Terminal
           color='yellow'
           backgroundColor='black'
           barColor='black'
-          style={{ fontWeight: "bold", fontSize: "1em" }}
+          style={{ fontWeight: "bold", fontSize: "1.3em" }}
           commandPassThrough={(cmd, print) => {
             this.sendToServer(cmd);
             if (this.state.data !== '') {
               print(this.state.data);
-              this.setState({data: ''})
+              this.setState({data: ''});
             }
-          }}
-          startState={'closed'}
+            }
+          }
+          startState={'maximised'}
           closedMessage={'Welcome to Jam Session! Click the icon to connect to the server'}
           closedTitle={'Jam Session Landing Page'}
           msg='Jam Session (Python).'
         />
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div
+        style={this.renderSwitch()}
+      >
+        {(this.renderMainScreen()) || (this.renderLandingMessage())}
       </div>
     );
   }
