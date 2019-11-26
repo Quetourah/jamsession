2e0277d2f295b9b22d69e7880ec21458f7c16a7b
Excel Espina <espinaexcel@gmail.com>
2019-11-26 08:10:10 -0500

    support for sclang only

--- jamsession_core/server.py
+++ jamsession_core/server.py
@@ -16,15 +16,16 @@ def hello():
         value = request.values['code']
         if value is None:
             return jsonify({"msg": "Need the `code` present"}), 400
-        with open('tmp_comm.py', 'w+') as f:
+        with open('/tmp_comm.sc', 'w+') as f:
             f.truncate(0) # erase everything
-            f.write("import FoxDot\n")
+            # f.write("import FoxDot\n")
             f.write(value)
         
-        res = subprocess.call("python tmp_comm.py", shell=True)
+        
+        res = subprocess.call("xvfb-run -a sclang /tmp_comm.sc", shell=True)
 
         print("RES: {}".format(res))
-        return jsonify({'code':value})
+        return jsonify({'code':value}), 200
 
 if __name__ == "__main__":
     app.run()
--- jamsession_core/tmp_comm.py
+++ jamsession_core/tmp_comm.py
@@ -1,2 +1,2 @@
 import FoxDot
-print("HI")
+print("Hi")
