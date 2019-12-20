195e72c9eae17aaefc5cff3476f04149a38e3f1e
quetourah <dina.qd.academics@gmail.com>
2019-12-19 23:15:22 -0500

    Create _config.js
    add config file template

---
+++ frontend/app/src/_config.js
@@ -0,0 +1,17 @@
+export default {
+    cognito: {
+        REGION: "",
+        USER_POOL_ID:"",
+        APP_CLIENT_ID:"",
+        IDENTITY_POOL_ID: ""
+    },
+    graphql: {
+        URL: "",
+        REGION: "",
+        AUTHENTICATION_TYPE: ""
+      },
+    s3: {
+        REGION: "",
+        BUCKET: ""
+      }
+};
