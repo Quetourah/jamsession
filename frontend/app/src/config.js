2ee57774c125b63b53a6d82f1279f7b714ebc390
Quetourah <Quetourah@users.noreply.github.com>
2019-12-20 09:18:40 -0500

    Update config.js

--- frontend/app/src/config.js
+++ frontend/app/src/config.js
@@ -1,18 +1,18 @@
 //Add your database information to retrieve data
 export default {
     cognito: {
-        REGION: "",
-        USER_POOL_ID:"",
-        APP_CLIENT_ID:"",
-        IDENTITY_POOL_ID: ""
+        REGION: REGION,
+        USER_POOL_ID: USER_POOL_ID,
+        APP_CLIENT_ID: APP_CLIENT_ID,
+        IDENTITY_POOL_ID: IDENTITY_POOL_ID
     },
     graphql: {
-        URL: "",
-        REGION: "",
-        AUTHENTICATION_TYPE: ""
+        URL: URL,
+        REGION: REGION,
+        AUTHENTICATION_TYPE: AUTHENTICATION_TYPE
       },
     s3: {
-        REGION: "",
-        BUCKET: ""
+        REGION: REGION,
+        BUCKET: BUCKET
       }
 };
