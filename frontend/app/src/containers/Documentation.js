b793755286c99bad0afeaec81195919186426e19
Baivab Pokhrel <baivab.pokhrel@gmail.com>
2019-12-20 12:31:16 -0500

    documentation added

--- frontend/app/src/containers/Documentation.js
+++ frontend/app/src/containers/Documentation.js
@@ -5,13 +5,38 @@ export default function Documentation() {
   return (
     <div className="docs">
      <div className="usefullinks">
-      <h1>Links</h1>
+      <h1>Useful Links</h1>
+      <ul>
+        <li>
+          <a href="https://en.wikipedia.org/wiki/SuperCollider" target="_blank">What is Supercollider</a>
+          
+        </li>
+        <li>
+        <a href="https://supercollider.github.io/" target="_blank">Getting started with Supercollider</a>
+          
+          </li>
+          <li>
+          <a href="https://supercollider.github.io/examples/supercollider-code-examples.html" target="_blank">Running a Sample code</a>
+          
+          </li>
+          <li>
+          <a href="http://doc.sccode.org/" target="_blank">Supercollider Documentation</a>
+          </li>
+      </ul>
      </div>
-     <div className="guide">
+     {/* <div className="guide">
        <h1>Guide</h1>
-     </div>
+     </div> */}
      <div className="contact">
        <h1>Contact Us</h1>
+       <ul>
+         <li>
+           Email: jamsession@gmail.com
+         </li>
+         <li>
+           Phone: XXX-XXX-XXXX
+         </li>
+       </ul>
      </div>
     </div>
   );
